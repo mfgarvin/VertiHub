adff294a33e89254f3840d6997f322ab37741c32
Michael Garvin <mfjgarvin@gmail.com>
2016-02-22 20:21:03 -0500

    Bug fixes

--- led.py
+++ led.py
@@ -19,13 +19,15 @@ LED_INVERT = False #True to invert the signal (when using NPN transistor level s
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 
+forceupdate = 1
+
 def timeCheck():
 	currentTime = time.localtime()
 	return currentTime
 
 def updateClock():
 	global running
-	firstrun = 1
+	global forceupdate
 	hour = time.localtime().tm_hour
 	minute = time.localtime().tm_min
 	second = 'OFF'
@@ -33,7 +35,7 @@ def updateClock():
 #	print(time.localtime().tm_hour - 12, time.localtime().tm_min)
 	while running == 1:
 		actualTime = timeCheck()
-		if actualTime.tm_hour - hour == 1 or actualTime.tm_hour - hour == -11 or firstrun == 1: 
+		if actualTime.tm_hour - hour == 1 or actualTime.tm_hour - hour == -11 or forceupdate == 1: 
 			hour = actualTime.tm_hour
 			print('pre', hour)
 			if hour == 0:
@@ -52,6 +54,8 @@ def updateClock():
 				if hour_str[1] == 0:
 					strip.setPixelColor(hour_str[0] + 1, placeholder)
 					inuseHour = hour_str[0]
+					for x in range(1, 16):
+						strip.setPixelColor(inuseHour + 3 + int(minute_str[0]) + x, Color(0, 0, 0))
 				else:
 					for x in xrange(hour_str[1]):
 						strip.setPixelColor(hour_str[0] + 1, Color(0, 0, 0))
@@ -59,7 +63,7 @@ def updateClock():
 					inuseHour = hour_str[0] + 1 + hour_str[1]
 			print(hour)
 			#Here, add another pixil to the clock
-		if actualTime.tm_min - minute == 1 or actualTime.tm_min - minute == -59 or firstrun == 1:
+		if actualTime.tm_min - minute == 1 or actualTime.tm_min - minute == -59 or forceupdate == 1:
 			minute = actualTime.tm_min
 			minute_str = str(minute)
 			print(minute)
@@ -70,17 +74,18 @@ def updateClock():
 					strip.setPixelColor(x, Color(0, 0, 0))	#Clear the minutes
 			if 1 <= minute <= 9:
 				for x in xrange(minute):
-					strip.setPixelColor(inuseHour + x + 1, minuteColor)
+					strip.setPixelColor(inuseHour + x + 3, minuteColor)
 			if 10 <= minute:
-#Issue Here -------->				
 				for x in xrange(int(minute_str[0])):
 					strip.setPixelColor(inuseHour + x + 3, minuteColor)
-#				if int(minute_str[1]) == 0:
-#					strip.setPixelColor(inuseHour + int(minute_str[0]) + 2, placeholder)
-#				else:
-#					for x in xrange(int(minute_str[1])):
-#						strip.setPixelColor(inuseHour + int(minute_str[0]) + 1, Color(0, 0, 0))
-#						strip.setPixelColor(inuseHour + int(minute_str[1]) + x, minuteColor)
+				if int(minute_str[1]) == 0:
+					strip.setPixelColor(inuseHour + 3 + int(minute_str[0]), placeholder)
+					for x in range(1,10):
+						strip.setPixelColor(inuseHour + 3 + int(minute_str[0]) + x, Color(0, 0, 0))
+				else:
+					for x in xrange(int(minute_str[1])):
+						strip.setPixelColor(inuseHour + 3 +  int(minute_str[0]), Color(0, 0, 0))
+						strip.setPixelColor(inuseHour + 3 + int(minute_str[0]) + 1 + x, minuteColor)
 		if second == 'OFF':
 			#Turn LED On
 			print('on')
@@ -92,8 +97,8 @@ def updateClock():
 			second = 'OFF'
 			strip.setPixelColor(strip.numPixels() - 1, Color(0, 0, 0))
 		strip.show()
-		if firstrun == 1:
-			firstrun = 0
+		if forceupdate == 1:
+			forceupdate = 0
 		time.sleep(1)
 		#Check hour, minute, second
 
